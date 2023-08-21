f0bf95a0cefc100dbc745bc2282a2a35aede10ca
Chooks22 <49726759+Chooks22@users.noreply.github.com>
2023-08-21 22:43:47 +0800

    📦 NEW: add initial wayland stuff

---
+++ nvim/init.lua
@@ -0,0 +1,45 @@
+require "core"
+
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+
+if custom_init_path then
+  dofile(custom_init_path)
+end
+
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
+
+-- chooks: vim config
+vim.wo.relativenumber = true
+
+-- chooks: shortcuts
+-- vim.keymap.set("n", "<leader>e", builtin.)
+
+-- chooks: lsp-zero
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- When you don't have mason.nvim installed
+-- You'll need to list the servers installed in your system
+lsp.setup_servers({'tsserver', 'eslint'})
+
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()

723b02fc7cde6a2a752b366898e44bbe744a78d2
Chooks22 <49726759+Chooks22@users.noreply.github.com>
2023-09-01 17:23:29 +0800

    📦 NEW: add ctrl-p bind for searching files

--- nvim/init.lua
+++ nvim/init.lua
@@ -465,6 +465,8 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
+vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
